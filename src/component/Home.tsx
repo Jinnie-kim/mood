24f44fb4e8c3cf66f13ef2f9ebaf124698fcd09e
Jinnie <hjk7639@gmail.com>
2023-04-04 09:54:49 +0900

    로그인 커스텀 hook Home 컴포넌트에서 사용

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,7 +1,10 @@
+import useAuth from '../customHook/useAuth';
+
 type HomeProp = {
   code: string;
 };
 
 export default function Home({ code }: HomeProp) {
+  const accessToken = useAuth(code);
   return <p>{code}</p>;
 }
