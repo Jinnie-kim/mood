e074e2397672b7f8f6760cfb37c30f3073def21f
Jinnie <hjk7639@gmail.com>
2023-04-04 19:56:07 +0900

    스포티파이에 로그인 요청하는 AUTH_URL .env 파일로 분리

--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -1,8 +1,6 @@
 import { LoginButton } from './Login.styled';
-
-const AUTH_URL =
-  'https://accounts.spotify.com/authorize?client_id=6727280b598f42f18646ee89b28a2568&response_type=code&redirect_uri=http://127.0.0.1:5173/&scope=user-read-private%20user-read-email%20ugc-image-upload%20playlist-read-private%20playlist-modify-private%20playlist-modify-public%20user-read-recently-played%20user-top-read%20user-library-modify%20user-library-read';
+const { VITE_AUTH_URL } = import.meta.env;
 
 export default function Login() {
-  return <LoginButton href={AUTH_URL}>Login With Spotify 🎧</LoginButton>;
+  return <LoginButton href={VITE_AUTH_URL}>Login With Spotify 🎧</LoginButton>;
 }
