d6ced1b753206cd44da6f86da0c41134a73c529b
Jinnie <hjk7639@gmail.com>
2023-04-04 11:32:28 +0900

    스포티파이 로그인 시 보내는 데이터를 열람할 scope 추가 설정

--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -1,7 +1,7 @@
 import { LoginButton } from './Login.styled';
 
 const AUTH_URL =
-  'https://accounts.spotify.com/authorize?client_id=6727280b598f42f18646ee89b28a2568&response_type=code&redirect_uri=http://127.0.0.1:5173/&scope=user-read-private%20user-read-email';
+  'https://accounts.spotify.com/authorize?client_id=6727280b598f42f18646ee89b28a2568&response_type=code&redirect_uri=http://127.0.0.1:5173/&scope=user-read-private%20user-read-email%20ugc-image-upload%20playlist-read-private%20playlist-modify-private%20playlist-modify-public%20user-read-recently-played%20user-top-read%20user-library-modify%20user-library-read';
 
 export default function Login() {
   return <LoginButton href={AUTH_URL}>Login With Spotify 🎧</LoginButton>;
