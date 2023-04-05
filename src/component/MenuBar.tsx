16247ea8c73cad42dd4bf815e617c5b578dde2b4
Jinnie <hjk7639@gmail.com>
2023-04-06 00:09:20 +0900

    MenuBar Menu 버튼을 클릭하면 해당 페이지로 이동하도록 클릭이벤트 등록

--- src/component/MenuBar.tsx
+++ src/component/MenuBar.tsx
@@ -1,15 +1,30 @@
+import { useNavigate } from 'react-router-dom';
 import { MenuBars, MenuBarButton } from '../style/MenuBar.styled';
 
 const MenuBar = () => {
+  const navigate = useNavigate();
+
+  const moveToPlaylistHandler = () => {
+    navigate('/playlist');
+  };
+
+  const moveToMypageHandler = () => {
+    navigate('/mypage');
+  };
+
+  const moveToHomeHandler = () => {
+    navigate('/home');
+  };
+
   return (
     <MenuBars>
-      <li>
+      <li onClick={moveToHomeHandler}>
         <MenuBarButton>HOME</MenuBarButton>
       </li>
-      <li>
+      <li onClick={moveToPlaylistHandler}>
         <MenuBarButton>Playlist</MenuBarButton>
       </li>
-      <li>
+      <li onClick={moveToMypageHandler}>
         <MenuBarButton>Me</MenuBarButton>
       </li>
     </MenuBars>
