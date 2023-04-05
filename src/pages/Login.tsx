85ce62fdc5436ebee25b045e4b0926daab191413
Jinnie <hjk7639@gmail.com>
2023-04-06 00:35:25 +0900

    로그인 화면에서 accessToken 발급 여부를 확인하고 token이 있으면 /home으로 이동

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,25 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useToken, useGetToken } from '../store/store';
 import { LoginButton } from '../style/Login.styled';
+
 const { VITE_AUTH_URL } = import.meta.env;
 
 const Login = () => {
+  const token = useToken();
+  const getToken = useGetToken();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem('accessToken') !== null) {
+      getToken(localStorage.getItem('accessToken') as string);
+    }
+    if (token) {
+      navigate('/home');
+    }
+    console.log(token);
+  }, [token]);
+
   return <LoginButton href={VITE_AUTH_URL}>Login With Spotify 🎧</LoginButton>;
 };
 
