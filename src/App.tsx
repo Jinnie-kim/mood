6781cb454ca6260e52c5d0a7e8d9345286a7b1de
Jinnie <hjk7639@gmail.com>
2023-04-05 14:04:52 +0900

    최상단 App에 라우터 등록

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
+import { routers } from './router';
 import './App.css';
-import Login from './component/Login';
-import Home from './component/Home';
-import MenuBar from './component/MenuBar';
-
-const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
   return (
     <section className="App">
-      {code ? <Home code={code} /> : <Login />}
-      <MenuBar />
+      <RouterProvider router={routers} />
     </section>
   );
 }
