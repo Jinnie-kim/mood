f46176532119552b8781c8f9611a556e8bf68665
Jinnie <hjk7639@gmail.com>
2023-04-06 12:04:45 +0900

    스포티파이 유저 정보를 가져오는 api 작성 & api 요청 시 리턴하는 UserProfile정보 타입 설정

---
+++ src/api/user.ts
@@ -0,0 +1,9 @@
+export const getUserProfile = async (token: string): Promise<UserProfile> => {
+  const result = await fetch('https://api.spotify.com/v1/me', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return await result.json();
+};
