eec80b66389826754f9caaed8893b59a221d1e41
Jinnie <hjk7639@gmail.com>
2023-04-08 10:26:08 +0900

    유저의 top artist 데이터 last 1month 기준으로 가져와서 화면에 보여주기

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,69 @@
+import { useEffect, useState } from 'react';
+import { useToken, useGetToken } from '../store/store';
+import { getTopArtists, getTopTracks } from '../api/topItems';
+import { ToppageLayout, TopItemLayout, TopItemBox, TopItemLists, TopItemList, TermButtonBox, TermButton } from '../style/Home.styled';
+
 const Home = () => {
-  return <p>hi</p>;
+  const token = useToken();
+  const getToken = useGetToken();
+  const [topArtists, setTopArtists] = useState<TopArtists[]>();
+  const [topTracks, setTopTracks] = useState<TopTracks[]>();
+
+  useEffect(() => {
+    if (token) {
+      getTopArtists(token, 'short_term').then((res) => {
+        setTopArtists(res.items as TopArtists[]);
+      });
+      getTopTracks(token, 'short_term');
+      return;
+    }
+    if (localStorage.getItem('accessToken') !== null) {
+      getToken(localStorage.getItem('accessToken') as string);
+    }
+  }, [token]);
+
+  return (
+    <ToppageLayout>
+      <h1>My Listening History</h1>
+      <TopItemLayout>
+        {/* Top Artists */}
+        <TopItemBox>
+          <h1>Top Artists</h1>
+          <TermButtonBox>
+            <TermButton>Last 1 month</TermButton>
+            <TermButton>Last 6 month</TermButton>
+            <TermButton>All Time</TermButton>
+          </TermButtonBox>
+          <TopItemLists>
+            {topArtists?.map((topArtist) => {
+              return <TopItemList key={topArtist.id}>{topArtist.name}</TopItemList>;
+            })}
+          </TopItemLists>
+        </TopItemBox>
+        {/* Top Tracks */}
+        <TopItemBox>
+          <h1>Top Tracks</h1>
+          <TermButtonBox>
+            <TermButton>Last 1 month</TermButton>
+            <TermButton>Last 6 month</TermButton>
+            <TermButton>All Time</TermButton>
+          </TermButtonBox>
+          <TopItemLists>
+            <TopItemList>Hello - 소향</TopItemList>
+            <TopItemList>Until I found you - stephen sanchez, em beihold</TopItemList>
+            <TopItemList>Kiwi - Harry Styles</TopItemList>
+            <TopItemList>가을 - Darin</TopItemList>
+            <TopItemList>As it was - Harry styles</TopItemList>
+            <TopItemList>그대라는 시 - 태연</TopItemList>
+            <TopItemList>Music for a sushi restaurant - Harry styles</TopItemList>
+            <TopItemList>Late Night talking - Harry styles</TopItemList>
+            <TopItemList>Hard to love - blackpink</TopItemList>
+            <TopItemList>Love of my life - harry styles</TopItemList>
+          </TopItemLists>
+        </TopItemBox>
+      </TopItemLayout>
+    </ToppageLayout>
+  );
 };
 
 export default Home;
