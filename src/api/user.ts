a1e146a6dd929afcba5cbacb3becb65d211fcb4b
Jinnie <hjk7639@gmail.com>
2023-04-09 15:44:41 +0900

    accessToken의 validation을 체크하는 생성

--- src/api/user.ts
+++ src/api/user.ts
@@ -7,3 +7,18 @@ export const getUserProfile = async (token: string): Promise<UserProfile> => {
   });
   return await result.json();
 };
+
+export const isValidToken = async (token: string): Promise<boolean> => {
+  const result = await fetch('https://api.spotify.com/v1/me', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (result.status === 200) {
+    return true;
+  } else {
+    return false;
+  }
+};
